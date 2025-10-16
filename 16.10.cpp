9cb49dea131665fcabc61c3dcc6495a90b558261
disspaired <alehrom10092007@gmail.com>
2025-10-16 16:06:30 +0300

    16.10.cpp

---
+++ 16.10.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <io.h>
+#include <fcntl.h>
+#include <string>
+#endif
+int main(){
+#ifdef _WIN32
+    setlocale(LC_ALL, "");
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+#else
+    std::ios_base::sync_with_studio(false);
+    std::wcout.imbue(std::locale("en_US.UTF-8"));
+    std::wcin.imbue(std::locate("en_US.UTF-8"));
+#endif
+
+    wchar_t number;
+    int counter = 0;
+
+    while (counter < 10){
+        std::wcout << L"Введите число: ";
+        std::wcin >> number;
+
+        std::wcout << number<< std::endl;
+
+         ++counter;
+    }
+
+
+    std::wcout<<std::endl;
+
+    return 0;
+}
