3557bc59e26fdadda33f1a413f139cd06bc67edb
disspaired <alehrom10092007@gmail.com>
2025-10-20 10:32:12 +0300

    Create 20.10.2.cpp

---
+++ 20.10.2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <io.h>
+#include <fcntl.h>
+#include <string>
+#endif
+int main(){
+#ifdef _WIN32
+    setlocale(LC_ALL, "");
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+#else
+    std::ios_base::sync_with_studio(false);
+    std::wcout.imbue(std::locale("en_US.UTF-8"));
+    std::wcin.imbue(std::locate("en_US.UTF-8"));
+#endif
+
+    double price;
+
+    std::wcout << L"Ведите стоимость килограмма конфет: ";
+    std::wcin >> price;
+
+    int weight = 100;
+
+    while (weight <= 1000){
+        double cost = (price * weight) / 1000.0;
+        std::wcout <<weight <<L"г - "<< cost<< L" рублей" << std::endl;
+        weight += 100;
+
+    }
+
+    std::wcout<<std::endl;
+
+    return 0;
+}
